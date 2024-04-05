d183920ce50b122847cc1515c684afcded0db636
GttB96 <147666979+GttB96@users.noreply.github.com>
2024-04-05 11:04:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -69,6 +69,17 @@ def stations():
 def pie_chart_fuel():
     sql = """
     """
+@app.route('/historical.html')
+def historical_graph_by_year():
+    sql = """
+    SELECT state, year, electricchargingoutlets AS "electric_charging_outlets"
+    FROM all_historical
+    WHERE fueltypecode = 'ELEC'
+    ORDER BY state, year
+    """
+    result = db.session.execute(text(sql))
+    historical_data = [dict(row) for row in result.mappings()]
+    return render_template('historical.html', historical_data=historical_data)
     
 if __name__ == '__main__':
     app.run(debug=True)
